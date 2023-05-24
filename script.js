3777c9fa27b1608bc7d1ecbe685197b756d51902
DeBondor <75030704+DeBondor@users.noreply.github.com>
2023-05-24 18:12:29 +0200

    Added Styles CSS file and Script JS file

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    setTimeout(redirect, 2000);
+  };
+  
+  function redirect() {
+    window.location.href = "https://startit.bot/";
+  }
+  
+  function redirectToAnotherPage() {
+    window.location.href = "debondor.html";
+  }
